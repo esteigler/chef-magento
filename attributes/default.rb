79c5813bde7928fc0194dfc95595b1c098413903
Erik Steigler <erik.github@sublevo.com>
2013-07-25 20:31:48 -0400

    Lower the number of apache workers to support VM's with less memory

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,14 +17,14 @@ default['vagrant_magento']['config']['default_currency'] = "USD"           #requ
 default['vagrant_magento']['config']['db_host'] = "localhost"              #required, You can specify server port, ex.: localhost:3306
                                                                            #If you are not using default UNIX socket, you can specify it
                                                                            #here instead of host, ex.: /var/run/mysqld/mysqld.sock
-                                                                           
+
 default['vagrant_magento']['config']['db_model'] = "mysql4"                #Database type (mysql4 by default)
 default['vagrant_magento']['config']['db_name'] = "magento"                #required, Database Name
 default['vagrant_magento']['config']['db_user'] = "root"                   #required, Database User Name
 default['vagrant_magento']['config']['db_pass'] = "root"                   #required, Database User Password
 default['vagrant_magento']['config']['db_prefix'] = ""                     #optional, Database Tables Prefix
                                                                            #No table prefix will be used if not specified
-                                                                           
+
 default['vagrant_magento']['config']['session_save'] = "files"             #optional, where to store session data - in db or files. files by default
 default['vagrant_magento']['config']['admin_frontname'] = ""               #optional, admin panel path, "admin" by default
 default['vagrant_magento']['config']['url'] = "{{base_url}}"               #required, URL the store is supposed to be available at
@@ -32,12 +32,12 @@ default['vagrant_magento']['config']['skip_url_validation'] = "yes"        #opti
 default['vagrant_magento']['config']['use_rewrites'] = "yes"               #optional, Use Web Server (Apache) Rewrites,
                                                                            #You could enable this option to use web server rewrites functionality for improved SEO
                                                                            #Please make sure that mod_rewrite is enabled in Apache configuration
-                                                                           
+
 default['vagrant_magento']['config']['use_secure'] = "no"                  #optional, Use Secure URLs (SSL). Enable this option only if you have SSL available.
 default['vagrant_magento']['config']['secure_base_url'] = "{{base_url}}"   #optional, Secure Base URL
                                                                            #Provide a complete base URL for SSL connection.
                                                                            #For example: https://www.mydomain.com/magento/
-                                                                           
+
 default['vagrant_magento']['config']['use_secure_admin'] = "no"            #optional, Run admin interface with SSL
 default['vagrant_magento']['config']['enable_charts'] = ""                 #optional, Enables Charts on the backend's dashboard
 
@@ -61,3 +61,8 @@ override['mysql']['server_root_password'] = node['vagrant_magento']['config']['d
 
 node['mysql']['server_repl_password'] = "root"
 node['mysql']['server_debian_password'] = "root"
+
+# No need for the native number of apache workers
+override['apache']['prefork']['startservers'] = 4
+override['apache']['prefork']['minspareservers'] = 4
+override['apache']['prefork']['maxspareservers'] = 16
